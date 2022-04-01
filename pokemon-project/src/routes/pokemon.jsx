29ea7c03226e9790f07623e5b3fe0f95f6a6113c
Dylan de Groot <dylankoffiemok@gmail.com>
2022-04-01 17:31:57 +0200

    Fixed Object in Array inside Object

--- pokemon-project/src/routes/pokemon.jsx
+++ pokemon-project/src/routes/pokemon.jsx
@@ -4,10 +4,16 @@ import { GetPokemon } from "../data";
 export default function Pokemon() {
     let params = useParams();
     let pokemon = GetPokemon(parseInt(params.dexId, 10));
+    let typeList = [];
     return (
         <section className="pokemon-single">
             <h1>{pokemon.name}</h1>
-            <p>Type(s): {pokemon.type}</p>    
+            {pokemon.types.map((types) => (
+                <>
+                    {typeList.push(types.type)}
+                </>
+            ))}
+            <p>Type(s): {typeList.toString().replace(",", " + ")}</p>
         </section>
-    ) 
+    )
 }
