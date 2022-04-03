a810b4a8cf8b4bfc2a2a4299b0b23e22f31b8bde
Dylan de Groot <dylankoffiemok@gmail.com>
2022-04-03 12:01:45 +0200

    Building test proejct with api

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
-import { render } from "react-dom";
-import { 
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import App from "./App";
-import Pokedex from "./routes/pokedex";
-import Pokemon from "./routes/pokemon";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const rootElement = document.getElementById("root");
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="pokedex" element={<Pokedex />}>
-          <Route path=":dexId" element={<Pokemon />} />
-        </Route>
-        <Route 
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>Nope nothing here!</p>
-            </main>
-          }
-        />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  rootElement
-);
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root'))
